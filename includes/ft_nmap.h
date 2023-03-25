bc3e1ea063f330584d567309c3b7086fb9bea71a
root <root@ASUS-TUF-KOLTOR>
2023-03-25 14:13:42 +0100

    little test

---
+++ includes/ft_nmap.h
@@ -0,0 +1,13 @@
+#ifndef FT_NMAP_H
+# define FT_NMAP_H
+
+//https://www.devdungeon.com/content/using-libpcap-c#intro
+
+#include <stdio.h>
+#include <time.h>
+#include <pcap.h>
+#include <netinet/in.h>
+#include <netinet/if_ether.h>
+
+
+#endif
