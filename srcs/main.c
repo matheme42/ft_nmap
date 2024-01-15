4408720ee6a1e636ab039ea3ce91aa98ce5aadec
Tacmath <me@me.com>
2024-01-15 13:45:01 +0100

    added filter

--- srcs/main.c
+++ srcs/main.c
@@ -19,6 +19,39 @@ void print_packet_info(const u_char *packet, struct pcap_pkthdr packet_header) {
     printf("Packet total length %d\n", packet_header.len);
 }
 
+void print_devs(pcap_if_t *alldevsp) {
+    char errbuf[PCAP_ERRBUF_SIZE];
+    bpf_u_int32 netp, maskp;
+    pcap_if_t *dev;
+
+    dev = alldevsp;
+    printf("devs:\n");
+    while (dev) {
+        if (pcap_lookupnet(dev->name, &netp, &maskp, errbuf)) {
+            printf("Error lookupnet: %s\n", errbuf);
+            dev = dev->next;
+            continue;
+        }
+        printf("name %s\n", dev->name);
+        printf("ip %d\n", netp);
+        printf("mask %d\n\n", maskp);
+        dev = dev->next;
+    }
+}
+
+void set_filter(pcap_t *p) {
+    struct bpf_program program;
+    const char *str = "greater 200";
+
+    if (pcap_compile(p, &program, str, 1, PCAP_NETMASK_UNKNOWN)) {
+        fprintf(stderr, "Could not compile program : %s\n", pcap_geterr(p));
+        return ;
+    }
+    if (pcap_setfilter(p, &program)) {
+        fprintf(stderr, "Could not set filter : %s\n", pcap_geterr(p));
+        return ;
+    }
+}
 
 int main(int argc, char *argv[]) {
     char *device;
@@ -31,7 +64,7 @@ int main(int argc, char *argv[]) {
         printf("Error finding devs: %s\n", error_buffer);
         return 1;
     }
-
+    print_devs(alldevsp);
 
     /* Open device for live capture */
     handle = pcap_open_live(
@@ -44,12 +77,13 @@ int main(int argc, char *argv[]) {
     if (handle == NULL) {
          fprintf(stderr, "Could not open device %s: %s\n", device, error_buffer);
          return 2;
-     }
+    }
 
     pcap_freealldevs(alldevsp);
 
+    set_filter(handle);
      
-    while (1) pcap_dispatch(handle, 0, my_packet_handler, NULL);
+    pcap_dispatch(handle, 0, my_packet_handler, NULL);
 
     pcap_close(handle);
     return 0;
