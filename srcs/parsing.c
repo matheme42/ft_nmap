9d413d8a7ef30178f737c505df346bafc4c8532a
wireshark <fake@fake.com>
2024-01-16 15:05:11 +0100

    add display data function

--- srcs/parsing.c
+++ srcs/parsing.c
@@ -211,11 +211,25 @@ void free_data(t_data *data) {
     free_tab(data->ip_address);
 }
 
+void print_data(t_data *data) {
+    dprintf(1, "speedup: %d\n", data->speedup);
+    dprintf(1, "mask: (%s %s %s %s %s %s)\n", data->scanmask.type.ack ? "ACK": "", data->scanmask.type.fin ? "FIN": "", data->scanmask.type.null ? "NULL": "", data->scanmask.type.syn ? "SYN": "", data->scanmask.type.udp ? "UDP": "", data->scanmask.type.xmas ? "XMAS": "");
+    dprintf(1, "port number: %d\n", data->ports_number);
+    char **str = data->ip_address;
+    int i = 0;
+    while(str[i]) {
+        dprintf(1, "%s ", str[i]);
+        i++;
+    }
+    dprintf(1, "\n");
+}
+
 int main(int ac, char **av) {
     t_data data;
 
     if (!parse_arguments(ac, av, &data)) return (1);
 
+    print_data(&data);
     // execute program
     free_data(&data);
     return (0);
