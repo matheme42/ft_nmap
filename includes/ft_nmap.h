507086a5d1f245f8f29e0a4b32ea042c935267ea
wireshark <fake@fake.com>
2024-01-30 16:41:38 +0100

    test

--- includes/ft_nmap.h
+++ includes/ft_nmap.h
@@ -162,5 +162,5 @@ void free_data(t_data *data);
 
 // ****************** OTHERS SECTIONS ****************** //
 
-void create_scan_packet(char *scan, char *src_host, char *dest_host, char *dest_port);
+void create_scan_packet(E_SCAN scan, struct sockaddr *src, struct sockaddr *dst, t_packet *pkt)
 #endif
