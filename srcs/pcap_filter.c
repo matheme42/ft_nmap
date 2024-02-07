33ed7709690b48d835597e0fc5ec0fcb682eb411
Tacmath <me@me.com>
2024-02-07 14:25:46 +0100

    small change

--- srcs/pcap_filter.c
+++ srcs/pcap_filter.c
@@ -6,8 +6,10 @@ void set_filter(pcap_t *p, thread_data *data) {
 
     sprintf(filter, "(icmp) || ((tcp || udp) && src portrange %d-%d)", data->ports[0], data->ports[data->nb_port - 1]);
     printf("filter: %s\n", filter);
-    if (pcap_compile(p, &filter_program, filter, 1, PCAP_NETMASK_UNKNOWN) == PCAP_ERROR)
-        fprintf(stderr, "Cant compile portrange rule %s, exiting program...\n", pcap_geterr(p));
+    if (pcap_compile(p, &filter_program, filter, 1, PCAP_NETMASK_UNKNOWN) == PCAP_ERROR) {
+        fprintf(stderr, "Cant compile portrange rule %s\n", pcap_geterr(p));
+        return;
+    }
 
     if (pcap_setfilter(p, &filter_program) < 0)
         fprintf(stderr, "Could not set filter : %s\n", pcap_geterr(p));
